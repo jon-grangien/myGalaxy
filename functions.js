219d17406b74d1f7248ea664a005627622991514
jonathan <jonathan.grangien@gmail.com>
2015-04-24 17:49:20 +0200

    Add some function descriptions

--- functions.js
+++ functions.js
@@ -24,6 +24,7 @@ function onWindowResize() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
+// Planet spawn (gui)
 function addPlanet(){
 	// Atmosphere
 	var atmosphereGeometry = new THREE.SphereGeometry( 11, 32, 32 );
@@ -84,6 +85,7 @@ function addPlanet(){
 	planetPaths.push(tempArray);
 }
 
+// Add orbit path torus about sun to planets
 function addOrbitPath(radius) {
 	var pathGeometry = new THREE.TorusGeometry( radius, 0.5, 16, 100 );
 	var pathMaterial = new THREE.ShaderMaterial( {
@@ -139,6 +141,7 @@ function updatePlanetTexture(textureName){
 	activePlanet.texture.needsUpdate = true;	//gives error output but works anyway
 }
 
+// Add moon to active planet (gui)
 function addMoon() {
 
 	var sphereGeometry = new THREE.SphereGeometry( 2, 32, 32 );
