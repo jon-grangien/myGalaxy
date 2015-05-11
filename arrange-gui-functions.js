60d876e4b3147bbb6ea819807cff35fde3173b7f
jonathan <jonathan.grangien@gmail.com>
2015-05-11 17:44:19 +0200

    arrange menus on create planet

--- arrange-gui-functions.js
+++ arrange-gui-functions.js
@@ -1,5 +1,8 @@
 function initiallyHideMenus() {
-	// Menus that aren't supposed to 
+	// Menus that aren't supposed to show initially (prestanda?)
+    $('#user_menu').hide();
+    $('#edit-planet-container').hide();
+    $('.bygg-main').hide();
 }
 
 function menusOnLogin() {
@@ -20,5 +23,6 @@ function menusOnLogout() {
 }
 
 function menusOnCreatePlanet() {
-	$('#add_planet_button').hide("fast");
+	$('#add_planet_button').hide();
+    $('#edit-planet-container').show("fast");
 }
