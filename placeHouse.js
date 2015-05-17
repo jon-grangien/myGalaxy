37e3a0cf1d3fad5918129b3d83c7aa7e6a6e558e
Simon Hedlund <simhe813@student.liu.se>
2015-05-17 20:14:40 +0200

    JSON-loader implementation started

--- placeHouse.js
+++ placeHouse.js
@@ -2,6 +2,9 @@ function showHouse(pos) {
 	if(jumpPlanetOk) {
 		console.log("show house planet");
 
+
+
+		/*
 		loader.load( "obj/house.obj", "obj/house.mtl", function(object){ 
 		activePlanet.children[5].add(object);
 		object.position.copy( pos );
@@ -30,12 +33,12 @@ function showHouse(pos) {
 		});
 		object.lookAt({ x: 0, y: 0, z: 0 });
 		
-		}, onProgress, onError);
+		}, onProgress, onError);*/
 		
 	} else if(jumpMoonOk) {
 		console.log("show house moon");
 
-		loader.load( "obj/house.obj", "obj/house.mtl", function(object){ 
+		/*loader.load( "obj/house.obj", "obj/house.mtl", function(object){ 
 		activeMoon.children[3].add(object);
 		object.position.copy( pos );
 		object.scale.x = 0.01;
@@ -64,7 +67,7 @@ function showHouse(pos) {
 		});
 		object.lookAt({ x: 0, y: 0, z: 0 });
 		
-		}, onProgress, onError);
+		}, onProgress, onError);*/
 	}
 	
 }
@@ -73,22 +76,34 @@ function createHouse(pos) {
 	console.log("create house planet");
 
 	if(jumpPlanetOk) {
-		loader.load( "obj/house.obj", "obj/house.mtl", function(object){ 
-			activePlanet.children[4].add(object);
-			activePlanet.children[5].remove(activePlanet.children[5].children[0]);
-			object.position.copy( pos );
-			object.scale.x = 0.01;
-			object.scale.y = 0.01;
-			object.scale.z = 0.01;
-			
-			object.lookAt({ x: 0, y: 0, z: 0 });
-			
-					
-		}, onProgress, onError);
+
+		loader.load("obj/volcano.js", 
+
+			function(geometry) {
+
+
+
+				object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture("obj/volcano.png")}));
+				activePlanet.children[4].add(object);
+				activePlanet.children[5].remove(activePlanet.children[5].children[0]);
+				object.position.copy( pos );
+				object.scale.set(0.5, 0.5, 0.5);
+				object.lookAt({ x: 0, y: 0, z: 0 });
+
+				atmosphere.receiveShadow = true;
+				atmosphere.castShadow = true;
+
+
+				meshes.push(object);
+				scene.add(object);
+
+
+		});
+
 	} else if(jumpMoonOk) {
 		console.log("create house moon");
 
-		loader.load( "obj/house.obj", "obj/house.mtl", function(object){ 
+		l/*oader.load( "obj/house.obj", "obj/house.mtl", function(object){ 
 			activeMoon.children[2].add(object);
 			activeMoon.children[3].remove(activeMoon.children[3].children[0]);
 			object.position.copy( pos );
@@ -99,7 +114,7 @@ function createHouse(pos) {
 			object.lookAt({ x: 0, y: 0, z: 0 });
 
 				
-		}, onProgress, onError);
+		}, onProgress, onError);*/
 	}
 
 }
