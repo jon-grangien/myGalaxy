74acdae2d997bb5129d2ec3f468076fdbeb76e43
Simon Hedlund <simhe813@student.liu.se>
2015-05-26 15:32:56 +0200

    bugfix 2

--- arrange-gui-functions.js
+++ arrange-gui-functions.js
@@ -34,6 +34,7 @@ function menusOnCreatePlanet() {
     $('.edit-created-planet-container').show("fast");
     $('#jump-planet-button').show();
     $("#jump-planet-moon-container").css({"right": "240px" });  //move out 
+
 }
 
 function menusOnSave() {
@@ -53,7 +54,8 @@ function menusOnEditPlanet() {
     $('#add-planet-button').hide();
     $('#edit-planet-button').hide();
     $('#edit-planet-tabs').show();
-    $("#jump-planet-moon-container").css({"right": "240px" });  //move out
+    $("#jump-planet-moon-container").hide();
+    //$("#jump-planet-moon-container").css({"right": "240px" });  //move out
 }
 
 function menusOnBuildPlanet() {
@@ -62,18 +64,22 @@ function menusOnBuildPlanet() {
     $('#edit-planet-button').hide();
     $('#accordion-container').show();
     $("#jump-planet-moon-container").css({"right": "240px" });  //move out
+    $("#jump-system-container").hide();
 }
 
 function menusOnEditDone() {
     $('#edit-planet-tabs').hide();
     $('#accordion-container').hide();
+    $("#jump-system-container").show();
     $("#jump-planet-moon-container").css({"right": "120px" });  //move in
 
     if(!jumpPlanetOk && !jumpMoonOk) {     //not viewing planet/moon
+        $("#jump-planet-moon-container").show();
         $('#edit-planet-button').show();
         $('#add-planet-button').show();
     } else {
         $('#build-planet-button').show();   //viewing planet/moon
+
     }
 }
 
@@ -83,10 +89,13 @@ function showButtonsForActivePlanet() {
         $('#edit-planet-button').show(); 
     }
 
+    $('#jump-planet-button').show();
+
     if( !$('#jump-planet-moon-container').is(":visible") ){
         $('#jump-planet-moon-container').show();
         $('#jump-moon-button').hide();  //hide until moon active
     }
+
 }
 
 function menusOnDeletePlanetSystemView() {
@@ -134,7 +143,7 @@ function buttonsOnViewSystem() {
 
     console.log("viewing system");
 
-    if(thereArePlanets) {
+    if(thereArePlanets && activePlanet) {
         $('#jump-planet-button').show();
         $('#edit-planet-button').show();
     }
