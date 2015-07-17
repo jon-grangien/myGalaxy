2420a454b9622517e865303b426f42c75a5b11cd
Jonathan <jonathan.grangien@gmail.com>
2015-07-17 20:31:57 +0200

    restructure ui functionality code

--- ui-functionality.js
+++ ui-functionality.js
@@ -1,89 +1,89 @@
-$(function() {
-	$( ".planet_radius_slider" ).slider({
-		min: 130,
-		max: 900,
-		value: 150,
-		step: 1,
-		slide: function(event, ui) {
-			// activePlanet.position.x = ui.value;  påverkar inte
+$(document).ready(function() {
+	$(function() {
+		$( ".planet_radius_slider" ).slider({
+			min: 130,
+			max: 900,
+			value: 150,
+			step: 1,
+			slide: function(event, ui) {
+				// activePlanet.position.x = ui.value;  påverkar inte
 
-			// Find planet's torus replace
-			for (var i = 0; i < planetPaths.length; ++i) {
-				if (planetPaths[i][0] == activePlanet) {
-					if(planetPaths[i][1]) {
-						sunSphere.remove(planetPaths[i][1]);
-						var newPath = addOrbitPath(ui.value);
-						sunSphere.add(newPath);
-						planetPaths[i][1] = newPath;
-						planetOrbitRadiuses[i][1] = ui.value;
-					} else {
-						console.log("error: path not found");
-					}
-				}
-			}
-		}
-	});
-});
-
-$(function() {
-	$( ".planet_size_slider" ).slider({
-		min: 1,
-		max: 4,
-		value: 1,
-		step: 0.01,
-		slide: function(event, ui) {
-			activePlanet.scale.x = ui.value;
-			activePlanet.scale.y = ui.value;
-			activePlanet.scale.z = ui.value;
-
-			// update planet size array
-			for (var i = 0; i < planetSizes.length; ++i) {
-				if(planetSizes[i][0] == activePlanet) {
-					planetSizes[i][1] = ui.value;
-				}
-			}
-		}
-
-		// change: function( event, ui ) {
-		// 	// update planet size array
-		// 	for (var i = 0; i < planetSizes.length; ++i) {
-		// 		planetSizes[i][1] = ui.value;
-		// 	}
-		// }
-	});
-});
-
-$(function() {
-	$( ".planet_rotation_slider" ).slider({
-		min: 0.01,
-		max: 0.25,
-		value: 0.01,
-		step: 0.01,
-		change: function(event, ui) {
-			for (var i = 0; i < planetSpeeds.length; ++i) {
-				if (planetSpeeds[i][0] == activePlanet) {	//where first element is active planet
-					if(planetSpeeds[i][1] != ui.value) {
-
-						if(planetSpeeds[i][4] == 0){
-							planetSpeeds[i][4] = planetSpeeds[i][1] * time;
+				// Find planet's torus replace
+				for (var i = 0; i < planetPaths.length; ++i) {
+					if (planetPaths[i][0] == activePlanet) {
+						if(planetPaths[i][1]) {
+							sunSphere.remove(planetPaths[i][1]);
+							var newPath = addOrbitPath(ui.value);
+							sunSphere.add(newPath);
+							planetPaths[i][1] = newPath;
+							planetOrbitRadiuses[i][1] = ui.value;
 						} else {
-							planetSpeeds[i][4] = planetSpeeds[i][4] + (time-planetSpeeds[i][3])*planetSpeeds[i][1];
+							console.log("error: path not found");
 						}
-						//Change third element to old rotationspeed.
-						planetSpeeds[i][2] = planetSpeeds[i][1];
-						//Save the point of time as the fourth element.
-						planetSpeeds[i][1] = ui.value;		//change second element to slider value
-						planetSpeeds[i][3] = time ;
+					}
+				}
+			}
+		});
+	});
+
+	$(function() {
+		$( ".planet_size_slider" ).slider({
+			min: 1,
+			max: 4,
+			value: 1,
+			step: 0.01,
+			slide: function(event, ui) {
+				activePlanet.scale.x = ui.value;
+				activePlanet.scale.y = ui.value;
+				activePlanet.scale.z = ui.value;
+
+				// update planet size array
+				for (var i = 0; i < planetSizes.length; ++i) {
+					if(planetSizes[i][0] == activePlanet) {
+						planetSizes[i][1] = ui.value;
 					}
 				}
 			}
 
-			// updatePlanet();
-		}
+			// change: function( event, ui ) {
+			// 	// update planet size array
+			// 	for (var i = 0; i < planetSizes.length; ++i) {
+			// 		planetSizes[i][1] = ui.value;
+			// 	}
+			// }
+		});
 	});
-});
 
-$(document).ready(function(){
+	$(function() {
+		$( ".planet_rotation_slider" ).slider({
+			min: 0.01,
+			max: 0.25,
+			value: 0.01,
+			step: 0.01,
+			change: function(event, ui) {
+				for (var i = 0; i < planetSpeeds.length; ++i) {
+					if (planetSpeeds[i][0] == activePlanet) {	//where first element is active planet
+						if(planetSpeeds[i][1] != ui.value) {
+
+							if(planetSpeeds[i][4] == 0){
+								planetSpeeds[i][4] = planetSpeeds[i][1] * time;
+							} else {
+								planetSpeeds[i][4] = planetSpeeds[i][4] + (time-planetSpeeds[i][3])*planetSpeeds[i][1];
+							}
+							//Change third element to old rotationspeed.
+							planetSpeeds[i][2] = planetSpeeds[i][1];
+							//Save the point of time as the fourth element.
+							planetSpeeds[i][1] = ui.value;		//change second element to slider value
+							planetSpeeds[i][3] = time ;
+						}
+					}
+				}
+
+				// updatePlanet();
+			}
+		});
+	});
+
 	$('.meteor-check').iCheck({
 		checkboxClass: 'icheckbox_square-yellow',
 		increaseArea: '20%' // optional
@@ -116,127 +116,108 @@ $(document).ready(function(){
 			}
 		}
 	});
-});
 
 
-$(function() {
-	$( "#moon-radius-slider" ).slider({
-		min: 20,
-		max: 40,
-		value: 20,
-		step: 1,
-		slide: function(event, ui) {
-			for (var i = 0; i < moonPaths.length; ++i) {
-				if (moonPaths[i][0] == activeMoon) {
-					moonOrbitRadiuses[i][1] = ui.value; 
-					activeMoon.parent.remove(moonPaths[i][1]); 
+	$(function() {
+		$( "#moon-radius-slider" ).slider({
+			min: 20,
+			max: 40,
+			value: 20,
+			step: 1,
+			slide: function(event, ui) {
+				for (var i = 0; i < moonPaths.length; ++i) {
+					if (moonPaths[i][0] == activeMoon) {
+						moonOrbitRadiuses[i][1] = ui.value; 
+						activeMoon.parent.remove(moonPaths[i][1]); 
 
-					var newPath = addMoonOrbitPath(ui.value);
-					activeMoon.parent.add(newPath);
-					moonPaths[i][1] = newPath;
-				}
-			}
-		}
-	});
-});
-
-$(function() {
-	$( "#moon-rotation-slider" ).slider({
-		min: 0.1,
-		max: 1,
-		value: 0.1,
-		step: 0.1,
-		slide: function(event, ui) {
-			for (var i = 0; i < moonSpeeds.length; ++i) {
-				if (moonSpeeds[i][0] == activeMoon) {	//where first element is active planet
-
-					if(moonSpeeds[i][1] != ui.value) {
-
-						if(moonSpeeds[i][4] == 0){
-							moonSpeeds[i][4] = moonSpeeds[i][1] * time;
-						} else {
-							moonSpeeds[i][4] = moonSpeeds[i][4] + (time-moonSpeeds[i][3])*moonSpeeds[i][1];
-						}
-						//Change third element to old rotationspeed.
-						moonSpeeds[i][2] = moonSpeeds[i][1];
-						//Save the point of time as the fourth element.
-						moonSpeeds[i][1] = ui.value;		//change second element to slider value
-						moonSpeeds[i][3] = time ;
+						var newPath = addMoonOrbitPath(ui.value);
+						activeMoon.parent.add(newPath);
+						moonPaths[i][1] = newPath;
 					}
 				}
 			}
-		}
+		});
 	});
-});
 
-$(document).ready(function() {     
+	$(function() {
+		$( "#moon-rotation-slider" ).slider({
+			min: 0.1,
+			max: 1,
+			value: 0.1,
+			step: 0.1,
+			slide: function(event, ui) {
+				for (var i = 0; i < moonSpeeds.length; ++i) {
+					if (moonSpeeds[i][0] == activeMoon) {	//where first element is active planet
+
+						if(moonSpeeds[i][1] != ui.value) {
+
+							if(moonSpeeds[i][4] == 0){
+								moonSpeeds[i][4] = moonSpeeds[i][1] * time;
+							} else {
+								moonSpeeds[i][4] = moonSpeeds[i][4] + (time-moonSpeeds[i][3])*moonSpeeds[i][1];
+							}
+							//Change third element to old rotationspeed.
+							moonSpeeds[i][2] = moonSpeeds[i][1];
+							//Save the point of time as the fourth element.
+							moonSpeeds[i][1] = ui.value;		//change second element to slider value
+							moonSpeeds[i][3] = time ;
+						}
+					}
+				}
+			}
+		});
+	});
+
     $('.earth-tex-img').click(function() {
         updatePlanetTexture('earthmap.jpg');
     });    
-});
 
-$(document).ready(function() {     
     $('.alien-tex-img').click(function() {
         updatePlanetTexture('alien.jpg');
-        console.log("alien clicked");
     });    
-});
 
-$(document).ready(function() {     
     $('.cloudy-tex-img').click(function() {
         updatePlanetTexture('cloudy.jpg');
     });    
-});
 
-$(document).ready(function() {     
     $('.desolate-tex-img').click(function() {
         updatePlanetTexture('desolate.png');
     });    
-});
 
-$(document).ready(function() {     
     $('.klendathu-tex-img').click(function() {
         updatePlanetTexture('klendathu.png');
     });    
-});
 
-$(document).ready(function() {     
     $('.sandy-tex-img').click(function() {
         updatePlanetTexture('sandy.jpg');
     });    
-});
 
-$(document).ready(function() {     
     $('.scarl-tex-img').click(function() {
         updatePlanetTexture('scarl.png');
     });    
-});
 
-$(document).ready(function() {     
     $('.steel-tex-img').click(function() {
         updatePlanetTexture('steeltexture.jpg');
     });    
-});
 
-$(function() {
-$( "#accordion" ).accordion({
-  collapsible: true,
-  active: false,
-  animate:'linear',
-  animate:300
-});
-});
+	$(function() {
+		$( "#accordion" ).accordion({
+		  collapsible: true,
+		  active: false,
+		  animate:'linear',
+		  animate:300
+		});
+	});
 
-$(function() {
-$( "#texture-accordion" ).accordion({
-  collapsible: true,
-  active: false,
-  animate:'linear',
-  animate:300
-});
-});
+	$(function() {
+		$( "#texture-accordion" ).accordion({
+		  collapsible: true,
+		  active: false,
+		  animate:'linear',
+		  animate:300
+		});
+	});
 
-$(document).ready(function() {     
     $('#add-planet-button').click(function() {
         if(user) {
         	deselect();
@@ -252,10 +233,7 @@ $(document).ready(function() {
 	        menusOnCreatePlanet();
 	    }
     });    
-});
 
-
-$(document).ready(function() {     
     $('#music-button').click(function() {
        if(!musicOn){
        	    song1.play();
@@ -267,9 +245,7 @@ $(document).ready(function() {
        		musicOn = false;
        }
     });    
-});
 
-$(document).ready(function() {     
     $('#sound-button').click(function() {
         if(!soundOn){
         		soundOn = true;
@@ -279,18 +255,13 @@ $(document).ready(function() {
         		document.getElementById("sound-check-icon").className = "fa fa-volume-off fa-fw fa-1x";
         }
     });    
-});
 
-
-$(document).ready(function() {     
     $('#add-moon-button').click(function() {
         // selectPlanetsOk = false;   går inte än
         addMoon();
         buttonsOnAddMoon();
     });    
-});
 
-$(document).ready(function() {     
     $('#save_planet_button').click(function() {
     	var name = $('#planet-name-field').val();
 
@@ -305,9 +276,7 @@ $(document).ready(function() {
 		menusOnPlanetActive();
 		updatePlanetInDB();
     });    
-});
 
-$(document).ready(function() {     
     $('#edit-planet-button').click(function() {
         
     	if(user && editAccess) {
@@ -358,9 +327,7 @@ $(document).ready(function() {
 	    	console.log("user has no access to edit this planet");
 	    }
     });    
-});
 
-$(document).ready(function() {     
     $('.delete-planet-button').click(function() {
     	deletePlanetFromDB();	//before removePlanet() which sets activePlanet to null
         removePlanet();	//removing all planets false
@@ -372,40 +339,29 @@ $(document).ready(function() {
     	} else {
         	menusOnDeletePlanetSystemView();
     	}
-
-    	
-
     });    
-});
 
-$(document).ready(function() {     
     $('#jump-planet-button').click(function() {
     	if(!jumpInAction) {
 			planetBools();
 		    buttonsOnViewPlanet();
 		}
     });    
-});
 
-$(document).ready(function() {     
     $('#jump-moon-button').click(function() {
     	if(!jumpInAction) {
 	    	moonBools();
 	        buttonsOnViewMoon();
 	    }
     });    
-});
 
-$(document).ready(function() {     
     $('#jump-system-button').click(function() {
     	if(!jumpInAction) {
 	    	solarBools();
 	        buttonsOnViewSystem();
 	    }
     });    
-});
 
-$(document).ready(function() {     
     $('#build-planet-button').click(function() {
     	
     	if(user && editAccess) {
@@ -417,21 +373,17 @@ $(document).ready(function() {
 	    	console.log(user.getUsername() + " has no access to edit this planet");
 	    }
     });    
-});
 
-$(document).ready(function() {     
     $('.edit-done-button').click(function() {
         selectPlanetsOk = true;
         menusOnEditDone();
         updatePlanetInDB();
     });    
-});
 
-$(function() {
-	$( "#edit-planet-tabs" ).tabs();
-});
+	$(function() {
+		$( "#edit-planet-tabs" ).tabs();
+	});
 
-$(document).ready(function() {     
 	$("#jump-system-button").mouseup(function(){
 	    $(this).blur();
 	})
@@ -471,18 +423,14 @@ $(document).ready(function() {
 	$("#sound-button").mouseup(function(){
 	    $(this).blur();
 	})
-});
 
-$(document).ready(function(){
-  $(".room-container").slideupmenu({
-  	slideUpSpeed: 150, 
-  	slideDownSpeed: 200, 
-  	ease: "easeOutQuad", 
-  	stopQueue: true
-  });  
-});
+	$(".room-container").slideupmenu({
+		slideUpSpeed: 150, 
+		slideDownSpeed: 200, 
+		ease: "easeOutQuad", 
+		stopQueue: true
+	});  
 
-$(document).ready(function() {
     $('#solar-room').click(function() {
     	changeSolarSystem("Solar System");
     });  
